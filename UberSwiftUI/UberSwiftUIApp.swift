b15e888d92763fb123ca63275d7d1e2d32c365d2
Usama Shafiq <usama.shafiq@techlogix.com>
2024-12-02 09:30:25 +0500

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by UsamaShafiq on 02/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
